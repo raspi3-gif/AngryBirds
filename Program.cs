388fa4ceafe50a5f6a0fa11df132dd182b1d9c81
Raspi <color.ui>
2021-12-10 16:05:51 +0100

    Version 1.0

--- Program.cs
+++ Program.cs
@@ -2,35 +2,90 @@
 using System.Linq;
 
 public class Joc{
-    
+
+    public void Marcador(string jugador1,string jugador2, int contJugador1,int contJugador2){
+        Console.WriteLine(jugador1+":"+contJugador1);
+        Console.WriteLine(jugador2+":"+contJugador2);
+    }
+
+    public void LimpiarPantalla(){
+        for (int i=0;i<20;i++){
+            Console.WriteLine();
+        }
+    }
+
     public void start(){
         string opcion;
+
         do{
             Console.WriteLine("Escoged número de Angry Birds: 1, 3 o 5");
             opcion= Console.ReadLine();
         }while(opcion!="1" && opcion!="3" && opcion!="5");
-        Jugador j1 = new Jugador();
-        j1.PreguntaNombre();
+
         int turnos= int.Parse(opcion);
+
+        Cartas cartas= new Cartas();
+
+        AngryBird [] ArrayCartas = cartas.ConstruirPajaros();
+
+        AngryBird [] mazoJugador = cartas.AsignarMazo(ArrayCartas,turnos);
+        AngryBird [] mazoEnemigo = cartas.AsignarMazo(ArrayCartas,turnos);
+
+        Jugador j1 = new Jugador(mazoJugador);
+        j1.PreguntaNombre();
+
+        Enemigo enemy= new Enemigo(mazoEnemigo);
+
+        int AngryEscogido1;
+
+        while (turnos!=0){
+            do{
+                AngryEscogido1= j1.EscogePajaro();
+            }while(AngryEscogido1==-1);
+
+            int AngryEscogido2=enemy.MaquinaEscoge();
+
+            Console.WriteLine("\nEnergia "+j1.GetNombre()+":");
+            Console.Write(mazoJugador[AngryEscogido1].GetEnergia());
+
+            Console.WriteLine("\nEnergia "+enemy.GetNombre()+":");
+            Console.Write(mazoEnemigo[AngryEscogido2].GetEnergia());
+
+            if (mazoJugador[AngryEscogido1].GetEnergia()>mazoEnemigo[AngryEscogido2].GetEnergia()){
+                Console.WriteLine("\nEste turno lo gana "+j1.GetNombre()+"\n");
+                j1.SetPuntuacion();
+            }else if (mazoJugador[AngryEscogido1].GetEnergia()<mazoEnemigo[AngryEscogido2].GetEnergia())
+            {
+                Console.WriteLine("\nEste turno lo gana "+enemy.GetNombre()+"\n");
+                enemy.SetPuntuacion();
+            }else
+            {
+                Console.WriteLine("\nEmpate! nadie suma punto\n");
+            }
+
+            Marcador(j1.GetNombre(),enemy.GetNombre(),j1.GetPuntuacion(),enemy.GetPuntuacion());
+            Console.ReadLine();
+
+            LimpiarPantalla();
+
+            turnos--;
+        }
+
+        Console.WriteLine("\nFin de la partida!");
+
+        if (j1.GetPuntuacion()>enemy.GetPuntuacion()){
+            Console.WriteLine("\n"+j1.GetNombre()+" ha ganado la partida!");
+        }else if (j1.GetPuntuacion()<enemy.GetPuntuacion())
+        {
+            Console.WriteLine("\n"+enemy.GetNombre()+" ha ganado la partida!");
+        }else
+        {
+            Console.WriteLine("\nEMPATE :O !");
+        }
+
+
+        
     }
-}
-
-public class Jugador{
-    private string nombre;
-    
-    AngryBird [] jugador1=AsignarPajaros(pajarillos,turnos);
-  
-
-    public void PreguntaNombre(){
-        Console.WriteLine("Nombre del jugador:");
-        nombre =  Console.ReadLine();
-    }
-    
-}
-
-public class Enemigo{
-    private string nombreMaquina = "Roberto";
-     AngryBird [] jugador2=AsignarPajaros(pajarillos,turnos);
 }
 
 
@@ -52,86 +107,142 @@ public class AngryBird {
     public int GetEnergia(){
         return energia;
     }
+
+    public string GetNom(){
+        return nom;
+    }
  
 }
 
-public class CreaMazo{
-    private string [] pajaros={"rojo","amarillo","azul","verde","negro","blanco","naranja","rosa","rojo"};
-    private string [] nombres={"Red","Chuck","Jay Jake y Jim","Hal","Bomb","Matilda","Bubbles","Stella","Terence"};
-    private string [] especies={"cardenal","canario","azulejo","tucán","cuervo","gallina","gorrión","cacatua Galah","cardenal"};
-    private string [] poderes={"none","velocidad","dividirse en tres","efecto bumerang","explotar","lanzar huevo explosivo","hincharse","hacer burbujas","su peso"};
-    private int [] energia={2,23,64,45,67,91,13,31,44};
+public class Cartas{
+        private string [] nombres = {"Red","Chuck","Jay Jake y Jim","Hal","Bomb","Matilda","Bubbles","Stella","Terence"};
 
-    AngryBird [] pajarillos=ConstruirPajaros(nombres,especies,poderes,energia,turnos);
-    static void ConstruirPajaros(string [] nombres,string [] especies,string [] poderes,int [] energia,int turnos){
+        private string [] especies={"cardenal","canario","azulejo","tucán","cuervo","gallina","gorrión","cacatua Galah","cardenal"};
+
+        private string [] poderes={"none","velocidad","dividirse en tres","efecto bumerang","explotar","lanzar huevo explosivo","hincharse","hacer burbujas","su peso"};
+
+        private int [] energia={2,23,64,45,67,91,13,31,44};
+
+
+
+    
+    public AngryBird [] ConstruirPajaros(){
         AngryBird [] pajarillos= new AngryBird[9];
-        for (byte i=0;i<nombres.Length;i++){
-            pajarillos[i]=new AngryBird(nombres[i],especies[i],poderes[i],energia[i]);
+        for (byte i=0;i<this.nombres.Length;i++){
+            pajarillos[i]=new AngryBird(this.nombres[i],this.especies[i],this.poderes[i],this.energia[i]);
         }
+        return pajarillos;
     }
+
+    public AngryBird [] AsignarMazo(AngryBird [] pajaros,int turnos){
+        Random NumRandom = new Random();
+        int [] NumRandoms=new int[turnos];
+
+        for (int i=0;i<turnos;i++){
+            NumRandoms[i]=NumRandom.Next(0,8);
+        }
+
+        AngryBird [] pajaritos= new AngryBird[turnos];
+
+        for (int i=0;i<turnos;i++){
+            pajaritos[i]=pajaros[NumRandoms[i]];
+        }
+
+        return pajaritos;
+    }
+
+
 }
 
+public class Jugador{
+    private string nombre;
 
-class Program {
-    static void Main(string[] args){
-    }
+    private int puntuacion=0;
+
+    AngryBird [] Mazo;
     
-}
-    
-    static AngryBird [] AsignarPajaros(AngryBird [] pajaros,int turnos){
-            Random NumRandom = new Random();
-            int [] NumRandoms=new int[turnos];
-
-            for (int i=0;i<turnos;i++){
-                NumRandoms[i]=NumRandom.Next(0,8);
-            }
-
-            AngryBird [] pajaritos= new AngryBird[turnos];
-
-            for (int i=0;i<turnos;i++){
-                pajaritos[i]=pajaros[NumRandoms[i]];
-            }
-
-            return pajaritos;
+    public Jugador(AngryBird [] Mazo){
+        this.Mazo=Mazo;
     }
-    static int EscogePajaro(AngryBird [] jugador,string nombreJugador){
-        Console.WriteLine("\nTe toca "+nombreJugador+"\n");
-        foreach (AngryBird element in jugador){
-                Console.WriteLine(element.nom + "    |  energia: " + element.energia);
+
+    public void PreguntaNombre(){
+        Console.WriteLine("Nombre del jugador:");
+        nombre =  Console.ReadLine();
+    }
+
+    public int EscogePajaro(){
+        Console.WriteLine("\nTe toca "+this.nombre+"\n");
+        foreach (AngryBird element in this.Mazo){
+                Console.WriteLine(element.GetNom()+ "    |  energia: " + element.GetEnergia() );
         }
+
         Console.WriteLine("Escoge que Angry Bird quieres usar: ");
         string nom = Console.ReadLine();
-        for (int i=0;i<jugador.Length;i++){
-            if (jugador[i].nom==nom){
+        for (int i=0;i<this.Mazo.Length;i++){
+            if (this.Mazo[i].GetNom()==nom){
                 return i;
             }
         }
         return -1;
     }
 
-    static void BorrarPajaro(AngryBird [] jugador,int pajaroEscogido){
-        jugador[pajaroEscogido].nom="";
+    public string GetNombre(){
+        return nombre;
     }
-    static int MaquinaEscoge (AngryBird [] jugador){
+
+    public void SetPuntuacion(){
+        puntuacion++;
+    }
+
+    public int GetPuntuacion(){
+        return puntuacion;
+    }
+    
+}
+
+
+public class Enemigo{
+    private string nombreMaquina = "C-3P0";
+    private int puntuacion=0;
+    AngryBird [] Mazo;
+    public Enemigo(AngryBird [] Mazo){
+        this.Mazo=Mazo;
+    }
+
+    public int MaquinaEscoge (){
         Random NumRandom = new Random();
-        int random=NumRandom.Next(0,jugador.Length);
+        int random=NumRandom.Next(0,this.Mazo.Length);
         return random;
     }
 
-    static void Marcador(string jugador1,string jugador2, int contJugador1,int contJugador2){
-        Console.WriteLine(jugador1+":"+contJugador1);
-        Console.WriteLine(jugador2+":"+contJugador2);
-    }
-
-    static void LimpiarPantalla(){
-        for (int i=0;i<20;i++){
-            Console.WriteLine();
-        }
+    public string GetNombre(){
+        return nombreMaquina;
     }
     
-        
+    public void SetPuntuacion(){
+        puntuacion++;
+    }
+
+    public int GetPuntuacion(){
+        return puntuacion;
+    }
+}
+
+
+class Program {
+    static void Main(string[] args){
+                
         Joc j = new Joc();
         j.start();
+        
+    }
+    
+}
+    
+
+
+  
+
         
 
        
@@ -140,63 +251,10 @@ class Program {
 
         
 
-        
+ 
 
-        int contJug1=0;
-        int contJug2=0;
-
-        int AngryEscogido1;
-
-        while (turnos!=0){
-            do{
-                AngryEscogido1=EscogePajaro(jugador1,nombre1);
-            }while(AngryEscogido1==-1);
-
-            int AngryEscogido2=MaquinaEscoge(jugador2);
-
-            Console.WriteLine("\nEnergia "+nombre1+":");
-            jugador1[AngryEscogido1].MostarEnergia();
-
-            Console.WriteLine("\nEnergia "+nombreMaquina+":");
-            jugador2[AngryEscogido2].MostarEnergia();
-
-            if (jugador1[AngryEscogido1].energia>jugador2[AngryEscogido2].energia){
-                Console.WriteLine("\nGana "+nombre1+"\n");
-                contJug1++;
-            }else if (jugador1[AngryEscogido1].energia<jugador2[AngryEscogido2].energia)
-            {
-                Console.WriteLine("\nGana "+nombreMaquina+"\n");
-                contJug2++;
-            }else
-            {
-                Console.WriteLine("\nEmpate! nadie suma punto\n");
-            }
-
-            jugador1 = jugador1.Where((source, index) =>index != AngryEscogido1).ToArray();
-            jugador2 = jugador2.Where((source, index) =>index != AngryEscogido2).ToArray();
-            
-            Marcador(nombre1,nombreMaquina,contJug1,contJug2);
-            Console.ReadLine();
-
-            LimpiarPantalla();
-
-            turnos--;
-        }
-
-        Console.WriteLine("\nFin de la partida!");
-        if (contJug1>contJug2){
-            Console.WriteLine("\nEsta vez gana "+nombre1+"!");
-        }else if (contJug1<contJug2)
-        {
-            Console.WriteLine("\nEsta vez gana "+nombreMaquina+"!");
-        }else
-        {
-            Console.WriteLine("\nEMPATE :O !");
-        }
-
-
-
+/*
+  static void BorrarPajaro(AngryBird [] jugador,int pajaroEscogido){
+        jugador[pajaroEscogido].nom="";
     }
-
-}
 */
